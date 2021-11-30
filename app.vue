01dc2db106d5f631b0c3f493ba6f778689e9e56d
comfuture <comfuture@gmail.com>
2021-11-30 21:36:38 +0900

    1 file modified

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app">
     <h2>Counter</h2>
     <p>You clicked {{ count }} times!</p>
     <button @click="inc">Click!</button>
@@ -9,3 +9,10 @@
 const count = ref<number>(0);
 const inc = () => count.value++;
 </script>
+<style lang="postcss">
+.app {
+  h2 {
+    @apply text-gray-600;
+  }
+}
+</style>
