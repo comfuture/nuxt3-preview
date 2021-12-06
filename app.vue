0bf68a5b4789f3d4960f2141c470745d3b334903
comfuture <comfuture@gmail.com>
2021-12-06 14:57:55 +0900

    1 file modified

--- app.vue
+++ app.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h2 class="text-green-500">Counter</h2>
+    <h2>Counter</h2>
     <p>You clicked {{ count }} times!</p>
     <button @click="inc">Click!</button>
   </div>
 </template>
 <script setup lang="ts">
-import '~/assets/css/tailwind.css';
 const count = ref<number>(0);
 const inc = () => count.value++;
 </script>
+<style lang="postcss"?
+h2 {
+  @apply text-lg text-red-500;
+}
+</style>
