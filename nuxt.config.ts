7727471c186bab84af508b6997a8601d9ff0f29c
comfuture <comfuture@gmail.com>
2021-12-12 15:31:14 +0900

    3 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3';
 
 defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
+  buildModules: [
+    '@nuxt/postcss8'
+  ],
   build: {
     postcss: {
       postcssOptions: {
